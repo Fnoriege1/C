b3eb64b2871f2180dda7018db8b1398093b97963
Fnoriege1 <flavien.norie@universite-paris-saclay.fr>
2023-12-14 16:19:53 +0100

    ex1012

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void limiter(int stop);
 
 int main()
 {
-    printf("Hello world !\n");
+    int s;
+
+    printf("Indiquer une valeur pour stopper (0-100) : ");
+    scanf("%d",&s);
+    limiter(s); //la fonction limiter prend la valeur de s.
     return 0;
+}
+
+void limiter(int stop)
+{
+    int x;
+
+    for(x=0; x<=100; x++)
+    {
+        printf("%d",x);
+        if(x==stop)
+        {
+            puts("Vous gagnez !");
+            return;
+        }
+    }
+    puts("Je gagne !");
 }
