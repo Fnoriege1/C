a6863513a3141343465c95c933036858d6802415
Fnoriege1 <flavien.norie@universite-paris-saclay.fr>
2023-12-14 15:17:19 +0100

    ex1001

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void prompt(); //declaration de la fonction
+void busy();
 
 int main()
 {
-    printf("Hello world !\n");
+    busy();
     return 0;
+}
+
+/*Fonction prompt*/
+
+void prompt()
+{
+    printf("C:\\Dos>");
+}
+
+/*Fonction busy*/
+
+void busy()
+{
+    int loop;
+    char input [32];
+
+    loop=0;
+    while(loop<5)
+    {
+        prompt();
+        fgets(input, 31, stdin);
+        loop++;
+    }
 }
