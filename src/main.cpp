9ea741f81d8c591ff16aa32291f81c74bd46769a
Fnoriege1 <flavien.norie@universite-paris-saclay.fr>
2023-12-14 16:05:01 +0100

    ex1009

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+float convertir(float f);
 
 int main()
 {
-    printf("Hello world !\n");
+    float temp_f,temp_c;
+
+    printf("Temperature en Fahrenheit: ");
+    scanf("%f", &temp_f); //stoke dans la variable temp_f la valeur envoye au programme.
+    temp_c = convertir(temp_f);
+    printf("%.1fF vaut %.1fC\n", temp_f, temp_c);
     return 0;
+}
+
+float convertir(float f)
+{
+    return (f-32)/1.8;
 }
